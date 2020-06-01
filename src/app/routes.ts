6b965c9e81b6b5790977e06f13a7f5fc8e44287f
Damian Wygonik <damian.wygonik@gmail.com>
2020-06-01 10:27:00 +0200

    Added auth guard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,11 +3,25 @@ import { HomeComponent } from './home/home.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { MyRecipesListComponent } from './my-recipes-list/my-recipes-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'users', component: UsersListComponent },
-  { path: 'recipes', component: RecipesListComponent },
-  { path: 'myrecipes', component: MyRecipesListComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'users', component: UsersListComponent },
+      {
+        path: 'recipes',
+        component: RecipesListComponent,
+      },
+      {
+        path: 'myrecipes',
+        component: MyRecipesListComponent,
+      },
+    ],
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
