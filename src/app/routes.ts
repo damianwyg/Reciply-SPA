dca19d63f6b341c285f1cbc942606910e9bb8138
Damian Wygonik <damian.wygonik@gmail.com>
2020-06-03 15:07:09 +0200

    Renaming

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,21 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersListComponent } from './users/users-list/users-list.component';
-import { RecipesListComponent } from './recpies/recipes-list/recipes-list.component';
-import { MyRecipesListComponent } from './recpies/my-recipes-list/my-recipes-list.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { MyRecipesListComponent } from './recipes/my-recipes-list/my-recipes-list.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserDetailsComponent } from './users/user-details/user-details.component';
 import { UserDetailsResolver } from './_resolvers/user-details.resolver';
 import { UserListResolver } from './_resolvers/user-list.resolver';
 import { RecipesListResolver } from './_resolvers/recipes-list.resolver';
-import { RecipeDetailsComponent } from './recpies/recipe-details/recipe-details.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { RecipeDetailsResolver } from './_resolvers/recipe-details.resolver';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { UserEditResolver } from './_resolvers/user-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes';
 import { RecipeEditResolver } from './_resolvers/recipe-edit.resolver';
-import { RecipeEditComponent } from './recpies/recipe-edit/recipe-edit.component';
-import { RecipeAddComponent } from './recpies/recipe-add/recipe-add.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeAddComponent } from './recipes/recipe-add/recipe-add.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
