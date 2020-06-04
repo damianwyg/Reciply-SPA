19e8e85a46a0f5867928bac5117c9560e899b189
Damian Wygonik <damian.wygonik@gmail.com>
2020-06-04 15:00:40 +0200

    Changed register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,27 +12,69 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegistration = new EventEmitter();
-  model: any = {};
+  user: User;
+  registerForm: FormGroup;
 
   constructor(
     private authService: AuthService,
-    private alertify: AlertifyService
+    private router: Router,
+    private alertify: AlertifyService,
+    private fb: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createRegisterForm();
+  }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertify.success('Registration successful');
-      },
-      (error) => {
-        this.alertify.error(error);
-      }
-    );
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(
+        () => {
+          this.alertify.success('Registration successful');
+        },
+        (error) => {
+          this.alertify.error(error);
+        },
+        () => {
+          this.authService.login(this.user).subscribe(() => {
+            this.router.navigate(['/users']);
+          });
+        }
+      );
+    }
   }
 
   cancel() {
     this.cancelRegistration.emit(false);
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group(
+      {
+        username: ['', Validators.required],
+        displayName: ['', Validators.required],
+        email: ['', Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        avatarUrl: ['', Validators.required],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(8),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value
+      ? null
+      : { mismatch: true };
   }
 }
